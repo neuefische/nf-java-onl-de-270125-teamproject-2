68e2be66d42e3fc4780e9099bc92e9f1a93dcb38
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-12 16:27:03 +0100

    add integration test for @PostMapping.

--- backend/src/test/java/de/superfische/controller/WorkoutControllerTest.java
+++ backend/src/test/java/de/superfische/controller/WorkoutControllerTest.java
@@ -1,4 +1,59 @@
 package de.superfische.controller;
 
-public class WorkoutControllerTest {
+import de.superfische.repository.WorkoutRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class WorkoutControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    WorkoutRepository workoutRepository;
+
+    @Test
+    @DirtiesContext
+    void addWorkout() {
+        // given: Nothing but the class members
+
+        // when + then
+        try {
+            mockMvc.perform(MockMvcRequestBuilders
+                    .post("/api/workout")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content("""
+                            {
+                                 "id": "myTestID",
+                                 "description": "myTestDescription",
+                                 "workoutName": "myWorkoutName",
+                                 "imagePath": "c:/test/path/image.jpg"                            
+                            }
+                            """))
+                    .andExpect(MockMvcResultMatchers.status().isCreated())
+                    .andExpect(MockMvcResultMatchers.content().json("""
+                            {
+                                 "description": "myTestDescription",
+                                 "workoutName": "myWorkoutName",
+                                 "imagePath": "c:/test/path/image.jpg"                            
+                            }
+                            """))
+                    .andExpect(jsonPath("$.id").isNotEmpty());
+        } catch (Exception e) {
+            Assertions.fail();
+        }
+    }
+
 }
