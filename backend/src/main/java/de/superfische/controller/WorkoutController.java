71aa2e9e9af5750b18715813520d4918926ea1af
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-12 16:11:01 +0100

    add @PostMapping. remove unused import.

--- backend/src/main/java/de/superfische/controller/WorkoutController.java
+++ backend/src/main/java/de/superfische/controller/WorkoutController.java
@@ -1,11 +1,12 @@
 package de.superfische.controller;
 
+import de.superfische.model.Workout;
 import de.superfische.service.WorkoutService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +15,12 @@ public class WorkoutController {
 
     public WorkoutController(WorkoutService workoutService) {
         this.workoutService = workoutService;
+    }
+
+    @PostMapping("/workout")
+    @ResponseStatus(HttpStatus.CREATED) // 201 - Standard, when something was created
+    public Workout addWorkout(@RequestBody Workout workout) {
+        return workoutService.addWorkout(workout.description(), workout.workoutName(), workout.imagePath());
     }
 
     @DeleteMapping("/workout/{id}")
