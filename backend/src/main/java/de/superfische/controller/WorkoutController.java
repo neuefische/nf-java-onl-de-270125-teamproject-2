1eee32d086ba2817488b2d1d57d69a9f97a64e27
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-12 14:44:47 +0100

    setup for collaborative working

--- backend/src/main/java/de/superfische/controller/WorkoutController.java
+++ backend/src/main/java/de/superfische/controller/WorkoutController.java
@@ -1,4 +1,15 @@
 package de.superfische.controller;
 
+import de.superfische.service.WorkoutService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class WorkoutController {
+    private final WorkoutService workoutService;
+
+    public WorkoutController(WorkoutService workoutService) {
+        this.workoutService = workoutService;
+    }
 }
--- backend/src/main/java/de/superfische/service/WorkoutService.java
+++ backend/src/main/java/de/superfische/service/WorkoutService.java
@@ -1,4 +1,13 @@
 package de.superfische.service;
 
+import de.superfische.repository.WorkoutRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WorkoutService {
+    private final WorkoutRepository workoutRepository;
+
+    public WorkoutService(WorkoutRepository workoutRepository) {
+        this.workoutRepository = workoutRepository;
+    }
 }
