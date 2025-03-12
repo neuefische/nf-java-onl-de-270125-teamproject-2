2f1f96c6e20a464e0871c08a641cbcd1c18b4da4
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-12 15:52:59 +0100

    add IdService. add WorkoutService.addWorkout and some tests.

--- backend/src/test/java/de/superfische/service/WorkoutServiceTest.java
+++ backend/src/test/java/de/superfische/service/WorkoutServiceTest.java
@@ -1,4 +1,39 @@
 package de.superfische.service;
 
-public class WorkoutServiceTest {
+import de.superfische.model.IdService;
+import de.superfische.model.Workout;
+import de.superfische.repository.WorkoutRepository;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
+
+class WorkoutServiceTest {
+
+    WorkoutRepository mockWorkoutRepository = mock(WorkoutRepository.class);
+    WorkoutService workoutService = new WorkoutService(mockWorkoutRepository);
+
+    @Test
+    void addWorkout() {
+        // given
+        IdService idService = new IdService();
+        String id = idService.generateRandomID();
+        String description = "My Test description";
+        String workoutName = "My test workoutName";
+        String imagePath = "c:/test/path/image.jpg";
+        Workout workoutMocked = new Workout(id, description, workoutName, imagePath);
+        when(mockWorkoutRepository.insert(workoutMocked)).thenReturn(workoutMocked);
+
+        // when
+        Workout workoutInserted = workoutService.addWorkout(description, workoutName, imagePath);
+
+        // then
+        verify(mockWorkoutRepository).insert(workoutInserted);
+        assertNotNull(workoutInserted);
+        assertEquals(workoutMocked.description(), workoutInserted.description());
+        assertEquals(workoutMocked.workoutName(), workoutInserted.workoutName());
+        assertEquals(workoutMocked.imagePath(), workoutInserted.imagePath());
+        assertNotNull(workoutInserted.id());
+    }
 }
