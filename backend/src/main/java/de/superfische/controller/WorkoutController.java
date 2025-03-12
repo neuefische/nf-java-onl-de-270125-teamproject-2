72bacb0c5e57090a92ee92b5fe42e8f30b415b43
Bezi <bezi21@icloud.com>
2025-03-12 16:23:25 +0100

    Add delete workout functionality to API

--- backend/src/main/java/de/superfische/controller/WorkoutController.java
+++ backend/src/main/java/de/superfische/controller/WorkoutController.java
@@ -1,6 +1,9 @@
 package de.superfische.controller;
 
 import de.superfische.service.WorkoutService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +14,15 @@ public class WorkoutController {
 
     public WorkoutController(WorkoutService workoutService) {
         this.workoutService = workoutService;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteWorkout(@PathVariable String id) {
+        try {
+            workoutService.deleteWorkout(id);
+            return ResponseEntity.noContent().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- backend/src/main/java/de/superfische/service/WorkoutService.java
+++ backend/src/main/java/de/superfische/service/WorkoutService.java
@@ -10,4 +10,13 @@ public class WorkoutService {
     public WorkoutService(WorkoutRepository workoutRepository) {
         this.workoutRepository = workoutRepository;
     }
+
+    public void deleteWorkout(String id) {
+
+        if (!workoutRepository.existsById(id)) {
+            throw new IllegalArgumentException("Workout with ID " + id + " was not found."
+            );
+        }
+        workoutRepository.deleteById(id);
+    }
 }
