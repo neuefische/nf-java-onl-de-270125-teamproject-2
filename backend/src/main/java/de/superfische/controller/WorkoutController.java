bdd68032329ce403070fc28f20ec195849ea2309
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-12 16:11:01 +0100

    add @PostMapping. remove unused import.

--- backend/src/main/java/de/superfische/controller/WorkoutController.java
+++ backend/src/main/java/de/superfische/controller/WorkoutController.java
@@ -1,8 +1,9 @@
 package de.superfische.controller;
 
+import de.superfische.model.Workout;
 import de.superfische.service.WorkoutService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -11,5 +12,11 @@ public class WorkoutController {
 
     public WorkoutController(WorkoutService workoutService) {
         this.workoutService = workoutService;
+    }
+
+    @PostMapping("/workout")
+    @ResponseStatus(HttpStatus.CREATED) // 201 - Standard, when something was created
+    public Workout addWorkout(@RequestBody Workout workout) {
+        return workoutService.addWorkout(workout.description(), workout.workoutName(), workout.imagePath());
     }
 }
