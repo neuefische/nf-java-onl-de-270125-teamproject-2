b276ee18b281b33464ab53e00f08f86c6de642de
Manuel Simon <info@der-supernerd.de>
2025-03-14 20:30:21 +0100

    add header and home to frontend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
     return (
     <header>
 
-        <h1>Home</h1>
+        <h1>Super-Fish: Fitness from the Water!</h1>
         <nav className={"nav-bar"}>
             <button className={"nav-item"}>Home</button>
             <button className={"nav-item"}>All Workouts</button>
             <button className={"nav-item"}>Add Workout</button>
         </nav>
 
-        <img src={"https://www.der-supernerd.de/superfische-logo.webp"} className={"logo"} />
+        <img src={"https://www.der-supernerd.de/super-fische-logo.webp"} className={"logo"} />
     </header>)
 }
--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -5,8 +5,10 @@ export default function Home() {
     return (
         <>
             <Header />
+
+            <div className={"logo-container"}><img src={"https://www.der-supernerd.de/super-fische-logo.webp"} className={"logo"} alt={"Super-Fische - Logo"}/></div>
             <div className={"container-home-image"}>
-                <img src={"https://www.der-supernerd.de/home-pexels-rui-dias-469842-1472887.jpg"} />
+                <img src={"https://www.der-supernerd.de/home-pexels-rui-dias-469842-1472887.jpg"} alt={"Super-Fische - Home Titel Bild"} />
             </div>
         </>
     )
