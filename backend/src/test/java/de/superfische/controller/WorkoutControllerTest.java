2fc3725c648dd1b82504c98ac6ec835eb8cea779
Manuel Simon <info@der-supernerd.de>
2025-03-14 12:56:48 +0100

    Backend controller findWorkoutId including tests completed

--- backend/src/test/java/de/superfische/controller/WorkoutControllerTest.java
+++ backend/src/test/java/de/superfische/controller/WorkoutControllerTest.java
@@ -1,20 +1,23 @@
 package de.superfische.controller;
 
 import de.superfische.model.Workout;
-import de.superfische.repository.WorkoutRepository;
+import de.superfische.service.WorkoutService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.NoSuchElementException;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@WebMvcTest(WorkoutController.class)
 public class WorkoutControllerTest {
 
     @Autowired
@@ -64,7 +67,7 @@ public class WorkoutControllerTest {
         // WHEN
         mockMvc.perform(get("/api/workout/1"))
 
-        //THEN
+                //THEN
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                             {
@@ -77,6 +80,36 @@ public class WorkoutControllerTest {
     }
 
     @Test
+    void testFindWorkOutByid() throws Exception {
+        //GIVEN
+        Workout workout = new Workout("1", "Testdescription", "My Workoutname", "https://superfische.der-supernerd.de/image1.png");
+
+        //WHEN
+        when(workoutService.findWorkoutById("1")).thenReturn(workout);
+
+        //THEN
+        mockMvc.perform(get("/api/workout/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value("1"))
+                .andExpect(jsonPath("$.description").value("Testdescription"))
+                .andExpect(jsonPath("$.workoutName").value("My Workoutname"))
+                .andExpect(jsonPath("$.imagePath").value("https://superfische.der-supernerd.de/image1.png"));
+
+    }
+
+    @Test
+    void testFindWorkoutById() throws Exception {
+        //GIVEN
+
+        //WHEN
+        when(workoutService.findWorkoutById("1445")).thenThrow(new NoSuchElementException());
+
+        //THEN
+        mockMvc.perform(get("/api/workout/1445"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     @DirtiesContext
     void findWorkoutById_WhenWorkoutNotFound_thenStatus404() throws Exception {
         //GIVEN
@@ -84,8 +117,7 @@ public class WorkoutControllerTest {
         //WHEN
         mockMvc.perform(get("/api/workout/1"))
 
-        //THEN
+                //THEN
                 .andExpect(status().isNotFound());
     }
-
 }
