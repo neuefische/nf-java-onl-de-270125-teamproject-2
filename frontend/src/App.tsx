a2b175068f47d67d7d0084c53d4cbe5d3f093c53
GregorStallmeister <post@gregor-stallmeister.de>
2025-03-17 17:00:27 +0100

    solve warning.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,28 @@
-
-
+import './App.css'
+import {AddWorkout} from "./components/AddWorkout.tsx";
+import {Workout} from "./types/Workout.ts";
+import axios from "axios";
 import './css/App.css'
 import Home from "./components/Home.tsx";
 
-
-
 function App() {
+
+    function saveWorkout(workout: Workout) {
+        axios.post("/api/workout", workout)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch((errorResponse) => {
+                console.log(errorResponse)
+            })
+    }
+
     return (
+        <>
         <Home />
+            <AddWorkout saveWorkout={saveWorkout}/>
+        </>
+    )
+}
 
-
-
-    );
-};
-
-export default App;
+export default App
