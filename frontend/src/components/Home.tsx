b15bb28b0989746ebf94f52a3c1e574f3ead4d33
Manuel Simon <info@der-supernerd.de>
2025-03-17 13:20:59 +0100

    Frontend Home Design add image and logo

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -6,9 +6,9 @@ export default function Home() {
         <>
             <Header />
 
-            <div className={"logo-container"}><img src={"https://www.der-supernerd.de/super-fische-logo-transparent.webp"} className={"logo"} alt={"Super-Fische - Logo"}/></div>
+            <div className={"logo-container"}><img src={"./public/super-fische-logo-transparent.webp"} className={"logo"} alt={"Super-Fische - Logo"}/></div>
             <div className={"container-home-image"}>
-                <img src={"https://www.der-supernerd.de/home-pexels-rui-dias-469842-1472887.jpg"} alt={"Super-Fische - Home Titel - Bild"} />
+                <img src={"./public/home-pexels-rui-dias-469842-1472887.jpg"} alt={"Super-Fische - Home Titel - Bild"} />
             </div>
         </>
     )
