6bec41969e0ed06fae3019df791dde01d0bfbd79
Manuel Simon <info@der-supernerd.de>
2025-03-20 16:54:00 +0100

    add header button navigate

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,26 @@
 import "../css/Header.css"
+import {useNavigate} from "react-router";
 
 export default function Header() {
+    const navigate = useNavigate()
+
     return (
     <header>
 
         <h1 className={"slogan-title"}>Super-Fish: Fitness from the Water!</h1>
         <nav className={"nav-bar"}>
-            <button className={"nav-item"}>Home</button>
-            <button className={"nav-item"}>All Workouts</button>
-            <button className={"nav-item"}>Add Workout</button>
+
+            <button className={"nav-item"} onClick={
+                () => { navigate("/") }
+            }>Home</button>
+
+            <button className={"nav-item"} onClick={
+                () => navigate("/workout/")
+            }>All Workouts</button>
+
+            <button className={"nav-item"} onClick={
+                () => navigate("/addworkout/")
+            }>Add Workout</button>
         </nav>
 
     </header>)
